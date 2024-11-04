99da19e539875ad86fb08e513e52d2df3655453c
Wiktor Lutowski <wlutowski@s201.local>
2024-11-04 09:28:21 +0100

    caluclate buttons functionality

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -36,5 +36,104 @@ namespace Figury
             RectangleBTextBox.IsEnabled = true;
             RectangleShape.Height = 150;
         }
+
+        private void RectangleCircuitButton_Click(object sender, RoutedEventArgs e)
+        {
+            double a = Parse(RectangleATextBox.Text);
+            if (a == -1)
+                return;
+
+            if ((bool)SquareRadioButton.IsChecked)
+                RectangleCircuitTextBox.Text = (a * 4).ToString();
+            else
+            {
+                double b = Parse(RectangleBTextBox.Text);
+                if (b == -1)
+                    return;
+
+                RectangleCircuitTextBox.Text = (a * 2 + b * 2).ToString();
+            }
+        }
+
+        private void RectangleAreaButton_Click(object sender, RoutedEventArgs e)
+        {
+            double a = Parse(RectangleATextBox.Text);
+            if (a == -1)
+                return;
+
+            if ((bool)SquareRadioButton.IsChecked)
+                RectangleAreaTextBox.Text = (a * a).ToString();
+            else
+            {
+                double b = Parse(RectangleBTextBox.Text);
+                if (b == -1)
+                    return;
+
+                RectangleAreaTextBox.Text = (b * a).ToString();
+            }
+        }
+
+        private void TriangleCircuitButton_Click(object sender, RoutedEventArgs e)
+        {
+            double a = Parse(TriangleATextBox.Text);
+            if (a == -1)
+                return;
+
+            TriangleCircuitTextBox.Text = (a * 3).ToString();
+        }
+
+        private void TriangleAreaButton_Click(object sender, RoutedEventArgs e)
+        {
+            double a = Parse(TriangleATextBox.Text);
+            if (a == -1)
+                return;
+
+            TriangleAreaTextBox.Text = Math.Round(a * a * Math.Sqrt(3) / 4, 3).ToString();
+        }
+
+        private void CircleCircuitButton_Click(object sender, RoutedEventArgs e)
+        {
+            double r = Parse(CircleRTextBox.Text);
+            if (r == -1)
+                return;
+
+            CircleCircuitTextBox.Text = Math.Round(2 * Math.PI * r, 3).ToString();
+        }
+
+        private void CircleAreaButton_Click(object sender, RoutedEventArgs e)
+        {
+            double r = Parse(CircleRTextBox.Text);
+            if (r == -1)
+                return;
+
+            CircleAreaTextBox.Text = Math.Round(Math.PI * (r * r), 3).ToString();
+        }
+
+        private double Parse(string text)
+        {
+            if(text == "")
+            {
+                MessageBox.Show("Nie podano boku");
+                return -1;
+            }
+
+            double result;
+
+            if(double.TryParse(text, out result))
+            {
+                if(result <= 0)
+                {
+                    MessageBox.Show("Bok musi być liczbą dodatnią");
+                    return -1;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Bok nie jest liczbą");
+                return -1;
+            }
+
+            return result;
+        }
     }
 }
